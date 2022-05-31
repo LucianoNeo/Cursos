bda392f55e8e4ad2cade510a4e3d8c69afdcb5dd
LucianoNeo <tec.info.luciano@hotmail.com>
2022-05-31 11:53:50 -0300

    módulos-import-export

---
+++ Spread/modulos/funcoes.mjs
@@ -0,0 +1,20 @@
+function mostraIdade(nome,idade){
+    console.log(`A idade de ${nome} é ${idade}`)
+}
+
+function mostraCidade(nome,cidade){
+    console.log(`A cidade de ${nome} é ${cidade}`)
+}
+
+
+function mostraHobby(nome,hobby){
+    console.log(`O hobby de ${nome} é ${hobby}`)
+}
+
+
+export{
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
+
---
+++ Spread/modulos/main.mjs
@@ -0,0 +1,5 @@
+import * as FUNCOES from './funcoes.mjs'
+
+console.log(FUNCOES.mostraIdade("luciano",36))
+console.log(FUNCOES.mostraCidade("luciano","CUritiba"))
+console.log(FUNCOES.mostraHobby("luciano","games"))
