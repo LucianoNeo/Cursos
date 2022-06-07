0ae316c07a1bdeb24a3cfe41e86994dd19ec2acf
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-07 15:19:31 -0300

    INTERFACES E TYPES

---
+++ Spread/typescript/index.ts
@@ -0,0 +1,55 @@
+function soma(a: number,b: number){
+    return a + b
+}
+
+soma(2,7)
+
+
+//interfaces
+
+interface IAnimal{
+    nome: string
+    tipo: 'terrestre' | 'aquatico'
+    domestico: boolean
+}
+
+
+interface IFelino extends IAnimal{
+    visaoNoturna: boolean
+}
+
+interface ICanino extends IAnimal{
+    porte: 'pequeno' | 'medio' | 'grande'
+}
+
+const felino: IFelino = {
+    nome: "Leão",
+    tipo:'terrestre',
+    visaoNoturna: true,
+    domestico:false
+}
+
+
+// types
+
+//Define o tipo de interface que uma const pode ter
+
+type IDomestico = IFelino | ICanino
+
+const animal: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    porte: 'medio',
+    tipo:'terrestre'
+}
+
+
+// DEFINIR O TIPO DO INPUT PRO TS
+const input = document.getElementById('input') as HTMLInputElement
+
+
+// CAPTURAR TUDO O QUE ESTA SENDO DIGITADO DENTRO DO INPUT
+input.addEventListener('input', (event)=> {
+    const i = event.currentTarget as HTMLInputElement
+    console.log(i.value)
+})
