765a3bc06700e4831173f687376db39c98882c35
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-18 15:25:34 -0300

    renderizacao condicional

--- Spread/REACTJS/lesson1/src/index.js
+++ Spread/REACTJS/lesson1/src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import Button from './Button'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const buttonB = <button>Botao 2</button>
+// VARIAVEL PARA CONTROLE DO RENDER
+const ligaBotao = true
+
+const renderBotao1 = () =>(
+<div><Button onClick={()=> {alert('clicou')}} name="Botao 1" /></div>
+)
+
+const renderBotao2 = () =>(
+  <div>{buttonB} </div>
+  )
+
+//FUNCAO IF E ELSE TERNARIO CASO LIGABOTAO LIGADO LIGA 1 OU 2
+
 const Elemento1 =()=>{
   return(
   <div className='textoInicial'>
     <p>'Meu primeiro react app!'</p>
-    <Button onClick={()=> {alert('clicou')}} name="conteudo do botao" />
+    {ligaBotao ?  renderBotao1():renderBotao2()}  
   </div> 
   )
 }
