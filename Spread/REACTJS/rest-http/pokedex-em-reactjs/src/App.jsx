24e5fdaf33e267dc4de2f10a097e958ad71ecee0
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-28 16:13:56 -0300

    pokedex react

--- Spread/REACTJS/rest-http/pokedex-em-reactjs/src/App.jsx
+++ Spread/REACTJS/rest-http/pokedex-em-reactjs/src/App.jsx
@@ -1,16 +1,12 @@
-import react from 'react'
+import Busca from './Busca'
 import LoadPokemon from './LoadPokemon'
-
-
-
-
-
 
 
 function App() {
  return (
  <div>
   <h1>Pokédex do Neo</h1>
+  <Busca />
   <LoadPokemon />
  </div>
  )
---
+++ Spread/REACTJS/rest-http/pokedex-em-reactjs/src/Busca.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Busca() {
+    return(
+        <input type="text" name="busca" id="busca" placeholder='Procure o pokémon desejado'/>
+    )
+}
+
+export default Busca
