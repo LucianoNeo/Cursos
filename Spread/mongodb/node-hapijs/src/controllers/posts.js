64ed3422987dc2bdae392a55028267fd807310b4
LucianoNeo <tec.info.luciano@hotmail.com>
2022-07-05 19:07:19 -0300

    hapi

---
+++ Spread/mongodb/node-hapijs/src/controllers/posts.js
@@ -0,0 +1,76 @@
+import Boom from 'boom'
+
+class PostsController {
+
+    constructor(Posts) {
+        this.Posts = Posts;
+    }
+
+    async find(request) {
+        const { id } = request.params
+        const query = {}
+
+        if (id) {
+            query._id = id
+        }
+
+        try {
+
+            const posts = await this.Posts.find(query)
+            return { posts }
+            
+        } catch (error) {
+            return Boom.badRequest('Failed to find post')
+        }
+    }
+
+    async create(request, h, err) {
+        
+        try {
+
+            const post = new this.Posts(request.payload)
+            await post.save()
+
+            return h.response().code(201)
+        } catch (error) {
+            return Boom.badRequest(error)
+        }
+    }
+
+    async update(request, h) {
+        
+        const { id } = request.params
+
+        try {
+            const updatedpost = await this.Posts.findOneAndUpdate({ _id: id }, request.payload, {
+                new: true,
+            });
+
+            if (updatedpost) {
+                return h.response().code(200)
+            }
+
+            return Boom.badRequest('Could not update the post')
+
+            
+        } catch (error) {
+            return Boom.badRequest(error)
+        }
+    }
+
+    async delete(request, h) {
+        const { id } = request.params
+        try{
+            const deletedPost = await this.Posts.deleteOne({_id:id})
+            if(deletedPost){
+                return h.response().code(200)
+            }
+        }
+        catch(error){
+            return Boom.badRequest(error)
+        }
+        
+    }
+}
+
+export default PostsController
